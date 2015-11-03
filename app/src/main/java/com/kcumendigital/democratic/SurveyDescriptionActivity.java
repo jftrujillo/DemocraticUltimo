52b81f6ab760ed18e0c5011e635ba5a5d8c20692
Jhon Trujillo <jftrujillo@unicauca.edu.co>
2015-11-03 18:20:37 -0500

    cambios en git

---
+++ app/src/main/java/com/kcumendigital/democratic/SurveyDescriptionActivity.java
@@ -0,0 +1,13 @@
+package com.kcumendigital.democratic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveyDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_description);
+    }
+}
